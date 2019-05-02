effbfa09b6be2e7794936e48a31fe401bee0d5f6
Alan Gonçalves <alan.smak@gmail.com>
2019-05-02 12:42:05 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/alangoncalves/workshopmongo/Services/UserService.java
+++ src/main/java/com/alangoncalves/workshopmongo/Services/UserService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.query.UpdateDefinition;
 import org.springframework.stereotype.Service;
 
 import com.alangoncalves.workshopmongo.Services.exception.ObjectNotFoundException;
